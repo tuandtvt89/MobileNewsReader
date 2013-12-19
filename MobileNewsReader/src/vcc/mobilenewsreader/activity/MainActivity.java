d5f6f2b0b35667e6061de0385e04cddcddf6316c
Doan Anh Tuan <tuanda@ominext.com>
2013-12-19 15:25:16 +0700

    create base project

--- MobileNewsReader/src/vcc/mobilenewsreader/activity/MainActivity.java
+++ MobileNewsReader/src/vcc/mobilenewsreader/activity/MainActivity.java
@@ -1,27 +1,42 @@
 package vcc.mobilenewsreader.activity;
 
+import java.util.ArrayList;
+
 import vcc.mobilenewsreader.R;
-import vcc.mobilenewsreader.fragment.HomeFragment;
+import vcc.mobilenewsreader.adapter.NavDrawerListAdapter;
+import vcc.mobilenewsreader.model.NavDrawerItem;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends FragmentActivity {
-	public HomeFragment mHomeContent;
+
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	// nav drawer title
+	private CharSequence mDrawerTitle;
+
+	// used to store app title
+	private CharSequence mTitle;
+
+	// slide menu items
+	private String[] navMenuTitles;
+	private TypedArray navMenuIcons;
+
+	private ArrayList<NavDrawerItem> navDrawerItems;
+	private NavDrawerListAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle paramBundle) {
 		super.onCreate(paramBundle);
-		if (paramBundle != null) {
-			this.mHomeContent = ((HomeFragment) getSupportFragmentManager()
-					.getFragment(paramBundle, "mContent"));
-		}
-		if (this.mHomeContent == null) {
-			this.mHomeContent = new HomeFragment();
-		}
-		setContentView(R.layout.layout_content_frame);
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.content_frame, this.mHomeContent).commit();
+		setContentView(R.layout.activity_main);
+		mTitle = mDrawerTitle = getTitle();
 	}
 
 	@Override
--- MobileNewsReader/src/vcc/mobilenewsreader/fragment/HomeFragment.java
+++ MobileNewsReader/src/vcc/mobilenewsreader/fragment/HomeFragment.java
@@ -1,37 +1,14 @@
 package vcc.mobilenewsreader.fragment;
 
-import vcc.mobilenewsreader.activity.MainActivity;
 import vcc.mobilenewsreader.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TableRow;
 
 public class HomeFragment extends Fragment {
 	
-	  private String currentDetailUrl = null;
-	  private String currentHomeUrl = null;
-	  private ImageButton imbHomePage;
-	  public ImageView imvNotifyCache;
-	  public boolean isFirstLoad = true;
-	  public LinearLayout lilBtnClose;
-	  private LinearLayout lilLogoHeader;
-	  public Button mBtnComment;
-	  public Button mBtnShare;
-	  private MainActivity mHostActivity;
-	  TableRow.LayoutParams mLayoutParamsAutoFit;
-	  public LinearLayout mainMainMenuTab;
-	  public LinearLayout mainWebview;
-	  public LinearLayout pageDetail;
-	  public LinearLayout pageHome;
-	  public ProgressBar progressBarDetail;
 	  
 	public View onCreateView(LayoutInflater paramLayoutInflater, ViewGroup paramViewGroup, Bundle paramBundle) {
 		super.onCreateView(paramLayoutInflater, paramViewGroup, paramBundle);
