cb2d4d8b9cfe3aeca5d6e738560f9cfb980432c3
tuandtvt89 <tuandtvt89@gmail.com>
2014-01-05 16:46:59 -0800

    fix

---
+++ MobileNewsReader/src/tuanda/mobilenewsreader/model/NavDrawerItem.java
@@ -0,0 +1,78 @@
+package tuanda.mobilenewsreader.model;
+
+public class NavDrawerItem {
+	
+	private String title;
+	private String content;
+	private int icon;
+	private int forward;
+	private String count = "0";
+	// boolean to set visiblity of the counter
+	private boolean isCounterVisible = false;
+	
+	public NavDrawerItem(){}
+
+	public NavDrawerItem(String title, int icon){
+		this.title = title;
+		this.icon = icon;
+	}
+	public NavDrawerItem(String title, String content, int icon){
+		this.title = title;
+		this.icon = icon;
+		this.content = content;
+	}
+	public NavDrawerItem(String title, int icon, boolean isCounterVisible, String count){
+		this.title = title;
+		this.icon = icon;
+		this.isCounterVisible = isCounterVisible;
+		this.count = count;
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+	
+	public int getIcon(){
+		return this.icon;
+	}
+	
+	public String getCount(){
+		return this.count;
+	}
+	
+	public boolean getCounterVisibility(){
+		return this.isCounterVisible;
+	}
+	
+	public void setTitle(String title){
+		this.title = title;
+	}
+	
+	public void setIcon(int icon){
+		this.icon = icon;
+	}
+	
+	public void setCount(String count){
+		this.count = count;
+	}
+	
+	public void setCounterVisibility(boolean isCounterVisible){
+		this.isCounterVisible = isCounterVisible;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getForward() {
+		return forward;
+	}
+
+	public void setForward(int forward) {
+		this.forward = forward;
+	}
+}
